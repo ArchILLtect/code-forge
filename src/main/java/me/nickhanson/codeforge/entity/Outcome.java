cb7d9cbef8f9fd4b894499e63834dbdca9d861a4
ArchILLtect <nickhansonsr@gmail.com>
2025-10-14 21:41:21 -0500

    add: implement ChallengeService with DAO integration and add DrillItem and Submission entities

---
+++ src/main/java/me/nickhanson/codeforge/entity/Outcome.java
@@ -0,0 +1,12 @@
+package me.nickhanson.codeforge.entity;
+
+/**
+ * Represents the outcome of a test case evaluation.
+ */
+public enum Outcome {
+    CORRECT,
+    ACCEPTABLE,
+    INCORRECT,
+    SKIPPED
+}
+
