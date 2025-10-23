c9192d68a1af90dc82a8c2f0387c789f5c569db6
ArchILLtect <nickhansonsr@gmail.com>
2025-10-23 18:30:17 -0500

    update: extend CodeforgeApplication to support servlet initialization

--- src/main/java/me/nickhanson/codeforge/CodeforgeApplication.java
+++ src/main/java/me/nickhanson/codeforge/CodeforgeApplication.java
@@ -2,12 +2,19 @@ package me.nickhanson.codeforge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * The main entry point for the Codeforge application.
  */
 @SpringBootApplication
-public class CodeforgeApplication {
+public class CodeforgeApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CodeforgeApplication.class);
+    }
     /**
      * The main method to run the Spring Boot application.
      *
